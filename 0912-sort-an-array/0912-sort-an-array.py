b953857fa1346cb9df4d05542e1fb616b453463d
Suhun Kim <kksshh404@gmail.com>
2023-02-28 16:52:51 -0800

    Time: 2647 ms (19.33%), Space: 33.2 MB (6.04%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,44 +1,33 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        temp_arr = [0] * len(nums)
-        
-        def merge(left: int, mid: int, right: int):
-            start1 = left
-            start2 = mid + 1
-            n1 = mid - left + 1
-            n2 = right - mid
+        # Function to heapify a subtree (in top-down order) rooted at index i.
+        def heapify(n: int, i: int):
+            # Initialize largest as root 'i'.
+            largest = i;
+            left = 2 * i + 1
+            right = 2 * i + 2
+            # If left child is larger than root.
+            if left < n and nums[left] > nums[largest]:
+                largest = left
+            # If right child is larger than largest so far.
+            if right < n and nums[right] > nums[largest]:
+                largest = right
+            # If largest is not root swap root with largest element
+            # Recursively heapify the affected sub-tree (i.e. move down).
+            if largest != i:
+                nums[i], nums[largest] =  nums[largest], nums[i]
+                heapify(n, largest)
 
-            for i in range(n1):
-                temp_arr[start1 + i] = nums[start1 + i]
-            for i in range(n2):
-                temp_arr[start2 + i] = nums[start2 + i]
+        def heap_sort():
+            n = len(nums)
+            # Build heap; heapify (top-down) all elements except leaf nodes.
+            for i in range(n // 2 - 1, -1, -1):
+                heapify(n, i)
+            # Traverse elements one by one, to move current root to end, and
+            for i in range(n - 1, -1, -1):
+                nums[0], nums[i] = nums[i], nums[0]
+                # call max heapify on the reduced heap.
+                heapify(i, 0)
 
-            i, j, k = 0, 0, left
-            while i < n1 and j < n2:
-                if temp_arr[start1 + i] <= temp_arr[start2 + j]:
-                    nums[k] = temp_arr[start1 + i]
-                    i += 1
-                else:
-                    nums[k] = temp_arr[start2 + j]
-                    j += 1
-                k += 1
-
-            while i < n1:
-                nums[k] = temp_arr[start1 + i]
-                i += 1
-                k += 1
-            while j < n2:
-                nums[k] = temp_arr[start2 + j]
-                j += 1
-                k += 1
-
-        def merge_sort(left: int, right: int):
-            if left >= right:
-                return
-            mid = (left + right) // 2
-            merge_sort(left, mid)
-            merge_sort(mid + 1, right)
-            merge(left, mid, right)
-    
-        merge_sort(0, len(nums) - 1)
+        heap_sort()
         return nums
