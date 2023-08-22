8d939ae35e1f0192cccef793c4303162ad698008
Suhun Kim <kksshh404@gmail.com>
2023-08-22 14:56:57 -0700

    Time: 278 ms (24.35%), Space: 17.9 MB (47.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,11 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        cnt, idx = 0, 0
-        while cnt < len(nums):
-            if nums[idx] == 0:
-                nums.pop(idx)
-                nums.append(0)
-                idx -= 1
-            cnt += 1
-            idx += 1
+        n = len(nums)
+        count = 0
+        for i in range(n):
+            temp = nums.pop(count)
+            # print(temp, nums)
+            if temp == 0:
+                nums.append(temp)
+            else:
+                nums.insert(count, temp)
+                count += 1
