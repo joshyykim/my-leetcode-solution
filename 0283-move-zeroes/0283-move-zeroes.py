a63b083e6509b2973872000a8edc72ecff4545f6
Suhun Kim <kksshh404@gmail.com>
2023-08-22 15:00:30 -0700

    Time: 151 ms (74.59%), Space: 17.8 MB (84.88%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,5 +1,5 @@
-
-def moveZeroes(nums: List[int]) -> None:
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
         cnt, idx = 0, 0
         while cnt < len(nums):
             if nums[idx] == 0:
@@ -8,15 +8,3 @@ def moveZeroes(nums: List[int]) -> None:
                 idx -= 1
             cnt += 1
             idx += 1
-
-
-f = open("user.out", "w")
-for line in stdin:
-    nums = list(map(int, line.rstrip()[1:-1].split(',')))
-    moveZeroes(nums)
-    f.write("[")
-    for n in nums:
-        f.write(f"{n},")
-    f.seek(f.tell() - 1)
-    f.write("]\n")
-exit()
