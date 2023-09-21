595b1cd95878a448e909697da30fd366aaffcfd3
Suhun Kim <kksshh404@gmail.com>
2023-09-21 10:23:35 -0700

    Time: 36 ms (82.30%), Space: 16.2 MB (90.89%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,18 +5,35 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        ### method 1 (iterate twice, store all linked list in the list)
-        li = []
+        # ### method 1 (iterate twice, store all linked list in the list) Time: O(n), Space: O(n)
+        # li = []
+        # curr = head
+        # while curr:
+        #     li.append(curr)
+        #     curr = curr.next
+        # to_be_removed = li[-n]
+        # curr = head
+        # while curr:
+        #     if curr.next == to_be_removed:
+        #         curr.next = to_be_removed.next
+        #     if curr == to_be_removed and curr == head:
+        #         head = curr.next
+        #     curr = curr.next
+        # return head
+        
+        ### method 2 (recursion) 
+        def helper(head, curr):
+            if not curr:
+                return 1
+            nth_from_tail = helper(head, curr.next)
+            if nth_from_tail == n and curr == head:
+                return -1
+            elif nth_from_tail == n+1:
+                curr.next = curr.next.next
+            return nth_from_tail+1
+            
         curr = head
-        while curr:
-            li.append(curr)
-            curr = curr.next
-        to_be_removed = li[-n]
-        curr = head
-        while curr:
-            if curr.next == to_be_removed:
-                curr.next = to_be_removed.next
-            if curr == to_be_removed and curr == head:
-                head = curr.next
-            curr = curr.next
-        return head
+        if helper(head, curr) == -1:
+            return head.next
+        else:
+            return head
