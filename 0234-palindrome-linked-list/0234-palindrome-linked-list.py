04e7b03bf08ec2225c614209d603549b5ae7dd9a
Suhun Kim <kksshh404@gmail.com>
2023-09-21 14:06:07 -0700

    Time: 9600 ms (5.02%), Space: 49.3 MB (27.43%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,12 +5,26 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # ### method 1 (Using list) ###
-        li = []
-        curr = head
-        while curr:
-            li.append(curr.val)
-            curr = curr.next
-        # return li[:(len(li)+1)//2] == list(reversed(li[len(li)//2:]))
-        return li == list(reversed(li))
+        # # ### method 1 (Using list) ###
+        # li = []
+        # curr = head
+        # while curr:
+        #     li.append(curr.val)
+        #     curr = curr.next
+        # return li == list(reversed(li))
+    
         ### method 2 ###
+        node = head
+        li = []
+        li_2 = []
+        if node == None or node.next == None:
+            return True
+        while node.next != None:
+            li.append(node.val)
+            li_2.insert(0, node.val)
+            node = node.next
+        # print(li,li_2)
+        li.append(node.val)
+        li_2.insert(0, node.val)
+        print(li,li_2)
+        return li == li_2
