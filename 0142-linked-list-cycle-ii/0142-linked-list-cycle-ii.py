ae4b570c0bd8cf19320c609c72932368bc300123
Suhun Kim <kksshh404@gmail.com>
2023-09-23 20:30:56 -0700

    Time: 55 ms (45.84%), Space: 20.6 MB (46.94%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -6,12 +6,36 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ### method 1 (using set) ###
-        s = set()
+        # ### method 1 (using set) => Time: O(n), Space: O(n) ###
+        # s = set()
+        # curr = head
+        # while curr:
+        #     if curr in s:
+        #         return curr
+        #     s.add(curr)
+        #     curr = curr.next
+        # return None
+        
+#         ### method 2 (slow & fast, two pointers) ###
+#         if not head:
+#             return None
+#         slow, fast = head, head.next
+        
+#         while True:
+#             if not fast or not fast.next:
+#                 return None
+            
+#             if slow == fast:
+                
+            
+#             slow = slow.next
+#             fast = fast.next
+        
         curr = head
         while curr:
-            if curr in s:
+            if hasattr(curr, "visited"):
                 return curr
-            s.add(curr)
+            else:
+                setattr(curr, "visited", 0)
             curr = curr.next
         return None
