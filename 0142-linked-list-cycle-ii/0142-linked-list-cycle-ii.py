96352dbedba7a4010c57ea11dc98d5686ac6ebe5
Suhun Kim <kksshh404@gmail.com>
2023-09-23 20:32:50 -0700

    Time: 46 ms (89.75%), Space: 20.7 MB (16.24%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -1,41 +1,9 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # ### method 1 (using set) => Time: O(n), Space: O(n) ###
-        # s = set()
-        # curr = head
-        # while curr:
-        #     if curr in s:
-        #         return curr
-        #     s.add(curr)
-        #     curr = curr.next
-        # return None
-        
-#         ### method 2 (slow & fast, two pointers) ###
-#         if not head:
-#             return None
-#         slow, fast = head, head.next
-        
-#         while True:
-#             if not fast or not fast.next:
-#                 return None
-            
-#             if slow == fast:
-                
-            
-#             slow = slow.next
-#             fast = fast.next
-        
-        curr = head
-        while curr:
-            if hasattr(curr, "visited"):
-                return curr
-            else:
-                setattr(curr, "visited", 0)
-            curr = curr.next
+        li = set()
+        while head:
+            if id(head) in li:
+                return head
+            li.add(id(head))
+            head = head.next
         return None
