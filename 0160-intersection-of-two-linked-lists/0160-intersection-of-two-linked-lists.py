21077f33123033ba1003742b331aa534c9ee2323
Suhun Kim <kksshh404@gmail.com>
2023-09-24 20:20:44 -0700

    Time: 134 ms (28.82%), Space: 31.4 MB (83.06%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,15 +6,45 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        linked_list_set = set()
+#         linked_list_set = set()
+#         currA, currB = headA, headB
+        
+#         while currA:
+#             while currB:
+#                 linked_list_set.add(id(currB))
+#                 currB = currB.next
+#             if id(currA) in linked_list_set:
+#                 return currA
+#             else:
+#                 currA = currA.next
+#         return None => O(n*m), O(m)
+
+        n, m = 0, 0
         currA, currB = headA, headB
         
         while currA:
-            while currB:
-                linked_list_set.add(id(currB))
-                currB = currB.next
-            if id(currA) in linked_list_set:
-                return currA
-            else:
+            n += 1
+            currA = currA.next
+        
+        while currB:
+            m += 1
+            currB = currB.next
+            
+        move_forward = 0
+        currA, currB = headA, headB
+        
+        if n > m:
+            move_forward = n - m
+            for i in range(move_forward):
                 currA = currA.next
+        elif n < m:
+            move_forward = m - n
+            for i in range(move_forward):
+                currB = currB.next
+        
+        while currA:
+            if currA == currB:
+                return currA
+            currA, currB = currA.next, currB.next
+        
         return None
