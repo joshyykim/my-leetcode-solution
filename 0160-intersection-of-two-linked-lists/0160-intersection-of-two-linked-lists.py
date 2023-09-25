54ee835953d03258a878376bd686b17279ebcc19
Suhun Kim <kksshh404@gmail.com>
2023-09-24 20:27:46 -0700

    Time: 125 ms (66.84%), Space: 31.8 MB (32.25%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -19,32 +19,45 @@ class Solution:
 #                 currA = currA.next
 #         return None => O(n*m), O(m)
 
-        n, m = 0, 0
-        currA, currB = headA, headB
+#         n, m = 0, 0
+#         currA, currB = headA, headB
         
-        while currA:
-            n += 1
-            currA = currA.next
+#         while currA:
+#             n += 1
+#             currA = currA.next
         
-        while currB:
-            m += 1
-            currB = currB.next
+#         while currB:
+#             m += 1
+#             currB = currB.next
             
-        move_forward = 0
+#         move_forward = 0
+#         currA, currB = headA, headB
+        
+#         if n > m:
+#             move_forward = n - m
+#             for i in range(move_forward):
+#                 currA = currA.next
+#         elif n < m:
+#             move_forward = m - n
+#             for i in range(move_forward):
+#                 currB = currB.next
+        
+#         while currA:
+#             if currA == currB:
+#                 return currA
+#             currA, currB = currA.next, currB.next
+        
+#         return None
+
         currA, currB = headA, headB
-        
-        if n > m:
-            move_forward = n - m
-            for i in range(move_forward):
-                currA = currA.next
-        elif n < m:
-            move_forward = m - n
-            for i in range(move_forward):
-                currB = currB.next
-        
-        while currA:
-            if currA == currB:
-                return currA
-            currA, currB = currA.next, currB.next
-        
-        return None
+    
+        while currA != currB:
+            if not currA:
+                currA = headB
+            else:
+                if not currB:
+                    currB = headA
+                else:
+                    currA, currB = currA.next, currB.next
+            
+        return currA
