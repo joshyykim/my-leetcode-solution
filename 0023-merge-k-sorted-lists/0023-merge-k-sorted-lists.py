d0263117c7838952eb650e8fab5d37497373399c
Suhun Kim <kksshh404@gmail.com>
2023-10-15 16:42:01 -0700

    Time: 3085 ms (10.95%), Space: 19.9 MB (61.42%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,20 +5,49 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # method 1 store it into list (n log n)
-        merged_list = []
-        for li in lists:
-            node = li
-            while node:
-                merged_list.append(node.val)
-                node = node.next
-        merged_list.sort()
+#         # method 1 store it into list (n log n)
+#         merged_list = []
+#         for li in lists:
+#             node = li
+#             while node:
+#                 merged_list.append(node.val)
+#                 node = node.next
+#         merged_list.sort()
         
+#         new_node = ListNode()
+#         new_head = new_node
+        
+#         for val in merged_list:
+#             new_node.next = ListNode(val)
+#             new_node = new_node.next
+        
+#         return new_head.next
+
+        # method 2 iterate over all list (O(k*len(lists[i])))
         new_node = ListNode()
         new_head = new_node
         
-        for val in merged_list:
-            new_node.next = ListNode(val)
-            new_node = new_node.next
-        
+        while lists:
+            temp_min = pow(10,4)+1
+            idx = -1
+            to_pop = []
+            for i in range(len(lists)):
+                if not lists[i]:
+                    to_pop.append(i)
+                else:
+                    if lists[i].val < temp_min:
+                        temp_min = lists[i].val
+                        idx = i
+            if temp_min < pow(10,4)+1:
+                new_node.next = ListNode(temp_min)
+                new_node = new_node.next
+            
+            if lists[idx]:
+                lists[idx] = lists[idx].next
+            for j in range(len(to_pop)):
+                lists.pop(to_pop[j])
+                for k in range(j, len(to_pop)):
+                    to_pop[k] -= 1
+                    
         return new_head.next
+        
