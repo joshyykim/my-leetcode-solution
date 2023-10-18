6e639c0d0918872b3b9f4b8721e1e538a1e3114b
Suhun Kim <kksshh404@gmail.com>
2023-10-17 20:15:30 -0700

    Time: 68 ms (21.61%), Space: 17.9 MB (93.88%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,33 +10,52 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        li = [[]]
-        tree = [root]
-        depth = 0
+        # li = [[]]
+        # tree = [root]
+        # depth = 0
+        # length = 1
+        # prev_length = length
+        # while tree:
+        #     length -= 1
+        #     temp = tree.pop(0)
+        #     if not temp: break
+        #     li[depth].append(temp.val)
+        #     if temp:
+        #         tree.append(temp.left)
+        #         tree.append(temp.right)
+        #     if length == 0:
+        #         length = prev_length * 2
+        #         prev_length = length
+        #         depth += 1
+        #         li.append([])
+                
+#         li.pop(-1)
+#         new_node = Node()
+#         new_root = new_node
+#         for i in range(len(li)):
+#             for j in li[i]:
+#                 new_node.next = Node(j)
+#                 new_node = new_node.next
+#             if i < len(li)-1:
+#                 new_node.next = Node("#")
+#                 new_node = new_node.next
+#         return new_root.next
+
         length = 1
         prev_length = length
+        tree = [root]
+        
         while tree:
             length -= 1
             temp = tree.pop(0)
             if not temp: break
-            li[depth].append(temp.val)
-            if temp:
-                tree.append(temp.left)
-                tree.append(temp.right)
+            tree.append(temp.left)
+            tree.append(temp.right)
+            
             if length == 0:
                 length = prev_length * 2
                 prev_length = length
-                depth += 1
-                li.append([])
+            else:
+                temp.next = tree[0]
                 
-        li.pop(-1)
-        new_node = Node()
-        new_root = new_node
-        for i in range(len(li)):
-            for j in li[i]:
-                new_node.next = Node(j)
-                new_node = new_node.next
-            if i < len(li)-1:
-                new_node.next = Node("#")
-                new_node = new_node.next
-        return new_root.next
+        return root
