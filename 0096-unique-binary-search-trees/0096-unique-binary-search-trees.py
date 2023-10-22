fd0d699db085e843fcd7a4cf17675dec5224e459
Suhun Kim <kksshh404@gmail.com>
2023-10-22 16:10:32 -0700

    Time: 36 ms (71.99%), Space: 16.3 MB (42.60%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,4 +1,3 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        li = [0,1,2,5,14,42,132,429,1430,4862,16796,58786,208012,742900,2674440,9694845,35357670,129644790,477638700,1767263190]
-        return li[n]
+        return int(math.factorial(2*n) / (math.factorial(n+1) * math.factorial(n)))
