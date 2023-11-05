899bb004b480b26e8f231de0abe9eca234e743fb
Suhun Kim <kksshh404@gmail.com>
2023-11-05 14:40:37 -0800

    Time: 2247 ms (11.83%), Space: 28.3 MB (99.46%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -9,20 +9,18 @@ class Solution:
         
         while True:
             if player1 > player2:
-                arr.append(player2)
                 if player1 not in winner:
                     winner[player1] = 1
                 else:
                     winner[player1] += 1
             else:
-                arr.append(player1)
                 if player2 not in winner:
                     winner[player2] = 1
                 else:
                     winner[player2] += 1
                 player1 = player2
             
-            if winner[player1] == k:
+            if winner[player1] == k or not arr:
                 break
             player2 = arr.pop(0)
         return player1
