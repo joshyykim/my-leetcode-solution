492a5b65c8bcbf72c423f32d68d561d96ac4051a
Suhun Kim <kksshh404@gmail.com>
2023-11-05 15:31:12 -0800

    Time: 2193 ms (12.37%), Space: 28.1 MB (99.46%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -2,25 +2,19 @@ class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
         if k >= len(arr):
             return max(arr)
-        winner = {}
+        count = 0
         
         player1 = arr.pop(0)
         player2 = arr.pop(0)
         
         while True:
             if player1 > player2:
-                if player1 not in winner:
-                    winner[player1] = 1
-                else:
-                    winner[player1] += 1
+                count += 1
             else:
-                if player2 not in winner:
-                    winner[player2] = 1
-                else:
-                    winner[player2] += 1
+                count = 1
                 player1 = player2
             
-            if winner[player1] == k or not arr:
+            if count == k or not arr:
                 break
             player2 = arr.pop(0)
         return player1
