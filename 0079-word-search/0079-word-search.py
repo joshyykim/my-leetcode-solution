a5905e1ab4207a47b63656013af15f38015d9a12
Suhun Kim <kksshh404@gmail.com>
2023-11-06 09:24:04 -0800

    Time: 7449 ms (14.96%), Space: 16.4 MB (11.29%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,31 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def helper(row, col, idx, tracking):
+            tracking.append((row, col))
+            res = False
+            if idx < len(word) and board[col][row] == word[idx]:
+                if idx == len(word)-1:
+                    return True
+                if row > 0 and (row-1, col) not in tracking:
+                    _tracking = tracking[:]
+                    res = res or helper(row-1, col, idx+1, _tracking)
+                if col > 0 and (row, col-1) not in tracking:
+                    _tracking = tracking[:]
+                    res = res or helper(row, col-1, idx+1, _tracking)
+                if row < len(board[0])-1 and (row+1, col) not in tracking:
+                    _tracking = tracking[:]
+                    res = res or helper(row+1, col, idx+1, _tracking)
+                if col < len(board)-1 and (row, col+1) not in tracking:
+                    _tracking = tracking[:]
+                    res = res or helper(row, col+1, idx+1, _tracking)
+                return res
+            else:
+                return False
+        
+        for col in range(len(board)):
+            for row in range(len(board[0])):
+                if helper(row, col, 0, []):
+                    return True
+        
+        return False
+                
