6c9a678023b54d92578f1a344db655d95cdb5d74
Suhun Kim <kksshh404@gmail.com>
2023-11-08 10:09:43 -0800

    Time: 6355 ms (34.40%), Space: 16.3 MB (41.22%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,22 +1,18 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def helper(row, col, idx, tracking):
-            res = False
-            if (row, col) not in tracking:
-                tracking.add((row, col))
-                if idx < len(word) and board[col][row] == word[idx]:
-                    if idx == len(word)-1:
-                        return True
-                    if row > 0 and (row-1, col) not in tracking:
-                        res = res or helper(row-1, col, idx+1, tracking.copy())
-                    if col > 0 and (row, col-1) not in tracking:
-                        res = res or helper(row, col-1, idx+1, tracking.copy())
-                    if row < len(board[0])-1 and (row+1, col) not in tracking:
-                        res = res or helper(row+1, col, idx+1, tracking.copy())
-                    if col < len(board)-1 and (row, col+1) not in tracking:
-                        res = res or helper(row, col+1, idx+1, tracking.copy())
-                    return res
-            return False
+            if idx == len(word):
+                return True
+            if row < 0 or row >= len(board[0]) or col < 0 or col >= len(board) or board[col][row] != word[idx] or (row, col) in tracking:
+                return False
+            
+            tracking.add((row, col))
+            res = (helper(row-1, col, idx+1, tracking) or
+                   helper(row, col-1, idx+1, tracking) or
+                   helper(row+1, col, idx+1, tracking) or
+                   helper(row, col+1, idx+1, tracking))
+            tracking.remove((row, col))
+            return res
         
         for col in range(len(board)):
             for row in range(len(board[0])):
