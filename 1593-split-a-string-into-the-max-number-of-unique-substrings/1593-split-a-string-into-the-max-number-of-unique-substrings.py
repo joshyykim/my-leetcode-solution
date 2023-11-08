aba248ed785bcd2b2a2f5a35e138c4ad1bd2c4ee
Suhun Kim <kksshh404@gmail.com>
2023-11-08 10:22:16 -0800

    Time: 224 ms (63.23%), Space: 16.3 MB (64.13%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -1,36 +1,17 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
-#         split_string = {}
-#         start_idx = 0
-        
-#         while start_idx < len(s):
-#             temp = s[start_idx]
-#             if temp in split_string:
-#                 for j in range(start_idx+1, len(s)):
-#                     temp += s[j]
-#                     start_idx += 1
-#                     if temp not in split_string:
-#                         split_string[temp] = 1
-#                         break
-#             else:
-#                 split_string[temp] = 1
-#             start_idx += 1
-                
-#         print(split_string)
-#         return len(split_string)
-
-        def helper(d, s):
+        def helper(added, s):
             if len(s) == 0:
-                return len(d)
+                return len(added)
             
             temp = ""
             res = 0
             for i in range(len(s)):
                 temp += s[i]
-                if temp not in d:
-                    new_d = d.copy()
-                    new_d[temp] = 1
-                    res = max(res, helper(new_d, s[i+1:]))
+                if temp not in added:
+                    added.add(temp)
+                    res = max(res, helper(added, s[i+1:]))
+                    added.remove(temp)
             return res
         
-        return helper({}, s)
+        return helper(set(), s)
