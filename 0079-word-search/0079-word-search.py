b4f0d888e4c9127d3b6af4ea2e6660c026e06242
Suhun Kim <kksshh404@gmail.com>
2023-11-08 10:01:10 -0800

    Time: 7280 ms (16.80%), Space: 16.3 MB (74.55%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,33 +1,26 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def helper(row, col, idx, tracking):
-            if (row, col) not in tracking:
-                tracking[(row, col)] = 1
-            else:
-                return False
             res = False
-            if idx < len(word) and board[col][row] == word[idx]:
-                if idx == len(word)-1:
-                    return True
-                if row > 0 and (row-1, col) not in tracking:
-                    _tracking = tracking.copy()
-                    res = res or helper(row-1, col, idx+1, _tracking)
-                if col > 0 and (row, col-1) not in tracking:
-                    _tracking = tracking.copy()
-                    res = res or helper(row, col-1, idx+1, _tracking)
-                if row < len(board[0])-1 and (row+1, col) not in tracking:
-                    _tracking = tracking.copy()
-                    res = res or helper(row+1, col, idx+1, _tracking)
-                if col < len(board)-1 and (row, col+1) not in tracking:
-                    _tracking = tracking.copy()
-                    res = res or helper(row, col+1, idx+1, _tracking)
-                return res
-            else:
-                return False
+            if (row, col) not in tracking:
+                tracking.add((row, col))
+                if idx < len(word) and board[col][row] == word[idx]:
+                    if idx == len(word)-1:
+                        return True
+                    if row > 0 and (row-1, col) not in tracking:
+                        res = res or helper(row-1, col, idx+1, tracking.copy())
+                    if col > 0 and (row, col-1) not in tracking:
+                        res = res or helper(row, col-1, idx+1, tracking.copy())
+                    if row < len(board[0])-1 and (row+1, col) not in tracking:
+                        res = res or helper(row+1, col, idx+1, tracking.copy())
+                    if col < len(board)-1 and (row, col+1) not in tracking:
+                        res = res or helper(row, col+1, idx+1, tracking.copy())
+                    return res
+            return False
         
         for col in range(len(board)):
             for row in range(len(board[0])):
-                if helper(row, col, 0, {}):
+                if helper(row, col, 0, set()):
                     return True
         
         return False
