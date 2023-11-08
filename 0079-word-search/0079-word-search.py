9cc711cfbd9776f3f76e78f8b8a7597206890135
Suhun Kim <kksshh404@gmail.com>
2023-11-08 09:57:13 -0800

    Time: 6012 ms (44.12%), Space: 16.4 MB (10.52%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,22 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def helper(row, col, idx, tracking):
-            tracking.append((row, col))
+            if (row, col) not in tracking:
+                tracking[(row, col)] = 1
+            else:
+                return False
             res = False
             if idx < len(word) and board[col][row] == word[idx]:
                 if idx == len(word)-1:
                     return True
                 if row > 0 and (row-1, col) not in tracking:
-                    _tracking = tracking[:]
+                    _tracking = tracking.copy()
                     res = res or helper(row-1, col, idx+1, _tracking)
                 if col > 0 and (row, col-1) not in tracking:
-                    _tracking = tracking[:]
+                    _tracking = tracking.copy()
                     res = res or helper(row, col-1, idx+1, _tracking)
                 if row < len(board[0])-1 and (row+1, col) not in tracking:
-                    _tracking = tracking[:]
+                    _tracking = tracking.copy()
                     res = res or helper(row+1, col, idx+1, _tracking)
                 if col < len(board)-1 and (row, col+1) not in tracking:
-                    _tracking = tracking[:]
+                    _tracking = tracking.copy()
                     res = res or helper(row, col+1, idx+1, _tracking)
                 return res
             else:
@@ -24,7 +27,7 @@ class Solution:
         
         for col in range(len(board)):
             for row in range(len(board[0])):
-                if helper(row, col, 0, []):
+                if helper(row, col, 0, {}):
                     return True
         
         return False
