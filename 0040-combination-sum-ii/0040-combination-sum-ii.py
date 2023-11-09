eac2dbaf6fb82a35ab8161e4e78655b9907b897f
Suhun Kim <kksshh404@gmail.com>
2023-11-09 10:01:56 -0800

    Time: 217 ms (5.08%), Space: 23.2 MB (5.93%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -2,29 +2,25 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         seen_combination = set()
+        candidates.sort()
         
         if sum(candidates) < target:
             return res
         
         def helper(start_idx, combination):
-            list_combination = [int(_) for _ in combination.split(",") if _ != ""]
-            list_combination.sort()
-            temp_sum = sum(list_combination)
-            if temp_sum == target and list_combination not in res:
-                res.append(list_combination)
+            temp_sum = sum(combination)
+            if temp_sum == target:
+                res.append(combination.copy())
                 return;
             elif temp_sum > target:
                 return;
                     
             for i in range(start_idx, len(candidates)):
-                if combination == "":
-                    combination = str(candidates[i])
-                else:
-                    combination += "," + str(candidates[i])
-                if combination not in seen_combination:
+                combination.append(candidates[i])
+                str_comb = ",".join([str(_) for _ in combination])
+                if str_comb not in seen_combination:
                     helper(i+1, combination)
-                    seen_combination.add(combination)
-                combination = combination[:-(1+len(str(candidates[i])))]
-        
-        helper(0, "")
+                    seen_combination.add(str_comb)
+                combination.pop(-1)
+        helper(0, [])
         return res
