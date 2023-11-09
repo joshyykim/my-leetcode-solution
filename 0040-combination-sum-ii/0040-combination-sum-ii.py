61cf393e71b3618faf4bfe76ce7b0ebcf05df9f0
Suhun Kim <kksshh404@gmail.com>
2023-11-09 10:32:40 -0800

    Time: 226 ms (5.08%), Space: 22.9 MB (5.93%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -4,18 +4,23 @@ class Solution:
         seen_combination = set()
         candidates.sort()
         
-        def helper(start_idx, combination, _target):
-            if _target == 0:
+        # if sum(candidates) < target:
+        #     return res
+        
+        def helper(start_idx, combination):
+            temp_sum = sum(combination)
+            if temp_sum == target:
                 res.append(combination.copy())
+                return;
+            elif temp_sum > target:
                 return;
                     
             for i in range(start_idx, len(candidates)):
-                if i > start_idx and candidates[i] == candidates[i - 1]:
-                    continue
-                if -candidates[i] > _target:
-                    break
                 combination.append(candidates[i])
-                helper(i+1, combination, _target-candidates[i])
-                combination.pop()
-        helper(0, [], target)
+                str_comb = ",".join([str(_) for _ in combination])
+                if str_comb not in seen_combination:
+                    helper(i+1, combination)
+                    seen_combination.add(str_comb)
+                combination.pop(-1)
+        helper(0, [])
         return res
