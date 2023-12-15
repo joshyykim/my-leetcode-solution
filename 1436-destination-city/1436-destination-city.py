ce5ecdf1181c642ac0fbbb825948dc9f69b520a2
Suhun Kim <kksshh404@gmail.com>
2023-12-15 11:24:35 -0800

    Time: 64 ms (43.94%), Space: 16.3 MB (45.28%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,8 +1,12 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        starts = [paths[_][0] for _ in range(len(paths))]
-        dests = [paths[_][1] for _ in range(len(paths))]
-        for i in range(len(paths)):
-            if dests[i] not in starts:
-                return dests[i]
-        return None
+        d = {}
+        
+        for path in paths:
+            d[path[0]] = path[1]
+        
+        dest = paths[0][1]
+        while dest in d:
+            dest = d[dest]
+        
+        return dest
