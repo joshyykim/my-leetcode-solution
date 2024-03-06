04e4a0adc255b572f699ae95231b8272a80aa097
Suhun Kim <kksshh404@gmail.com>
2024-03-06 10:14:56 -0800

    Time: 48 ms (40.39%), Space: 17 MB (93.88%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        valid_characters = string.ascii_letters + string.digits
+        i = 0
+        while i < len(s):
+            if s[i] not in valid_characters:
+                s = s.replace(s[i], "")
+            else:
+                i += 1
+        return s[:] == s[::-1]
