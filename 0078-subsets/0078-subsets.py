0305ea98c50c5b4857ef7f6aead1772b75a09d92
Suhun Kim <kksshh404@gmail.com>
2024-05-10 11:10:15 -0700

    Time: 40 ms (34.14%), Space: 16.7 MB (77.62%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,3 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        return sum([list(itertools.combinations(nums, _)) for _ in range(len(nums)+1)], [])
+        res = []
+        
+        def helper(idx, sub):
+            res.append(sub)
+            for i in range(idx, len(nums)):
+                local = sub[:]
+                local.append(nums[i])
+                helper(i+1, local)
+        
+        helper(0, [])
+        print(res)
+        return res
