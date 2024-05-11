4924925ffefbc05422ae8f6015720fc7161bd607
Suhun Kim <kksshh404@gmail.com>
2024-05-11 00:45:42 -0700

    Time: 44 ms (32.06%), Space: 16.5 MB (96.41%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,3 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        
-        def helper(idx, per):
-            if idx == len(per):
-                res.append(per)
-                return;
-            for i in range(idx, len(per)):
-                per[idx], per[i] = per[i], per[idx]
-                local = per[:]
-                helper(idx+1, local)
-                
-        helper(0, nums)
-        return res
+        return permutations(nums)
