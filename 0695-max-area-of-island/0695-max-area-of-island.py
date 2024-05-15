745645f93855a7b61592869bdef2745a55780031
Suhun Kim <kksshh404@gmail.com>
2024-05-14 20:07:57 -0700

    Time: 120 ms (28.04%), Space: 18.1 MB (35.06%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,29 +1,27 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        def isFound(i, j):
-            for island in islands:
-                if (i, j) in island:
-                    return True
-            return False
-        
         def findGround(i, j):
-            if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or (i, j) in islands[-1]:
+            if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or (i, j) in found:
                 return;
             
             if grid[i][j] == 1:
-                islands[-1].add((i,j))
+                found.add((i,j))
                 findGround(i+1, j)
                 findGround(i, j+1)
                 findGround(i-1, j)
                 findGround(i, j-1)
             
-        islands = []
+        found = set()
+        res = 0
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    if not isFound(i, j):
-                        islands.append(set())
+                    if (i, j) not in found:
+                        before = len(found)
                         findGround(i,j)
+                        after = len(found)
+                        if res < after - before:
+                            res = after - before
                     
-        return max([len(_) for _ in islands] + [0])
+        return res
