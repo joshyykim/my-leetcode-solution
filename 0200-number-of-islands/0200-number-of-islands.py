3b681cb97806c88e4192658452680f0894dcaae9
Suhun Kim <kksshh404@gmail.com>
2024-05-14 20:03:18 -0700

    Time: 264 ms (30.14%), Space: 27.9 MB (8.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,14 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = []
-        idx = 0
-        
-        
-        def isFound(i, j):
-            for island in islands:
-                if (i, j) in island:
-                    return True
-            return False
+        found = set()
+        res = 0
         
         def findGround(i, j):
-            if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or (i, j) in islands[-1]:
+            if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or (i, j) in found:
                 return;
             
             if grid[i][j] == "1":
-                islands[-1].add((i,j))
+                found.add((i,j))
                 findGround(i+1, j)
                 findGround(i, j+1)
                 findGround(i-1, j)
@@ -25,8 +18,8 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
-                    if not isFound(i, j):
-                        islands.append(set())
+                    if (i, j) not in found:
+                        res += 1
                         findGround(i,j)
                     
-        return len(islands)
+        return res
