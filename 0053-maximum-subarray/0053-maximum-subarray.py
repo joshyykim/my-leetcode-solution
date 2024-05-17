e57e0be9d9745003c375968d0921efaa2b1670d1
Suhun Kim <kksshh404@gmail.com>
2024-05-17 15:42:54 -0700

    Time: 538 ms (49.09%), Space: 31.1 MB (16.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,14 +1,22 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        sub, res = nums[0], nums[0]
+        result = -(10 ** 5)
+        temp, partial = 0, -(10 ** 5)
         
-        for i in range(1, len(nums)):
-            if nums[i] > nums[i] + sub:
-                sub = nums[i]
-            else:
-                sub = nums[i] + sub
+        for i in range(len(nums)):
+            if nums[i] <= 0 and nums[i] > result:
+                result = nums[i]
                 
-            if sub > res:
-                res = sub
-        
-        return res
+            if nums[i] <= 0 and temp + nums[i] >= 0:
+                temp += nums[i]
+                partial = temp
+            elif nums[i] <=0 and temp + nums[i] < 0:
+                temp = 0
+            elif nums[i] > 0:
+                temp += nums[i]
+                partial = temp
+            
+            if partial > result:
+                result = partial
+                
+        return result
